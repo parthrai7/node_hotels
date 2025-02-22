b0ea4b2f508788e393dc187abdef052eb5f3e40c
parthrai <parthrai.pr7@gmail.com>
2025-02-22 12:24:07 +0530

    Fixed case-sensitive issue for menuitems model

--- db.js
+++ db.js
@@ -1,18 +1,32 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/dbs", {
+// Define the MongoDB connection URL
+const mongoURL = process.env.MONGODB_URL;
+
+// Set up MongoDB connection
+mongoose.connect(mongoURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => {
-    console.log("MongoDB connected successfully");
-}).catch(err => console.log("MongoDB connection error:", err));
+})
 
+// Get the default connection
+// Mongoose maintains a default connection object representing the MongoDB connection.
 const db = mongoose.connection;
 
-db.on("error", (err) => console.error("❌ MongoDB connection error:", err));
-db.on("disconnected", () => {
-    console.log("⚠️  Mongoose server disconnected. Reconnecting...");
-    mongoose.connect(mongoURL); // Auto-reconnect
+// Define event listeners for database connection
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB server');
 });
 
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+
+// Export the database connection
 module.exports = db;
--- server.js
+++ server.js
@@ -1,68 +1,36 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
+const passport = require('./auth');
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json()); //it automatically stores data in req.body
-
-const Person = require('./models/Person');
-const menuitems = require('./models/menuitems');
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
 
 
+// Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); // Move on to the next phase
+}
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false})
 
 app.get('/', function (req, res) {
-  res.send('Hello party')
-});
-
-
-
-app.post('/person', async (req, res) => {
-    try {
-        const data = req.body;
-
-       
-
-        const response = await Person.insertMany(data); // Insert multiple records
-        console.log("Data saved:", response);
-        res.status(200).json(response);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-app.get('/person', async (req, res) => {
-    try {
-        const data = await Person.find();
-        console.log("Data fetch:");
-        res.status(200).json(data);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-
-app.listen(3000, ()=>{
-    console.log("server 3000 is live");
+    res.send('Welcome to our Hotel');
 })
-app.post('/menuitems',async (req,res)=>{
-    try{
-        const menu = req.body;
-        const items = await menuitems.insertMany(menu);
-        console.log("Items Updated");
-        res.status(200).json(items);
-    }catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-app.get('/menuitems', async(req,res)=>
-{try{
-    const data = await menuitems.find();
-    console.log("Items Updated");
-        res.status(200).json(data);
-    }catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+
+// Import the router files
+const personRoutes = require('./routes/personRoutes');
+const menuItemRoutes = require('./routes/menuItemRoutes');
+
+// Use the routers
+app.use('/person', personRoutes);
+app.use('/menu', menuItemRoutes);
+  
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
